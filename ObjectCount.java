7b986269e300b3d2d7f39d4e6e02a5c145913778
Bushra Hossain <87435687+BushraOnika@users.noreply.github.com>
2021-07-26 22:41:35 +0600

    Add files via upload
    Bush(Bushra)

---
+++ ObjectCount.java
@@ -0,0 +1,15 @@
+package CSE215TheoryAssignment01;
+class CountObject{
+    static int count=0;
+
+    public CountObject() {
+        count++;
+    }
+}
+public class ObjectCount {
+    public static void main(String[] args) {
+     CountObject c = new CountObject();
+     CountObject b = new CountObject();
+        System.out.println("Number of objects  are : "+CountObject.count);
+    }
+}
